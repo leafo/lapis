b99698d48fd9f5e47bc5acd740274fdd76b875dc
leaf corcoran <leafot@gmail.com>
2012-12-03 01:38:53 -0800

    add basic validation module

---
+++ lapis/validate.lua
@@ -0,0 +1,114 @@
+local insert = table.insert
+local validate_functions = {
+  exists = function(input)
+    return input and input ~= "", "%s must be provided"
+  end,
+  min_length = function(input, len)
+    return #tostring(input or "") >= len, "%s must be at least " .. tostring(len) .. " chars"
+  end,
+  max_length = function(input, len)
+    return #tostring(input or "") <= len, "%s must be at most " .. tostring(len) .. " chars"
+  end,
+  is_integer = function(input)
+    return tostring(input):match("^%d+$"), "%s must be an integer"
+  end,
+  equals = function(input, value)
+    return input == value, "%s must match"
+  end
+}
+local test_input
+test_input = function(input, func, args)
+  local fn = assert(validate_functions[func], "Missing validation function " .. tostring(func))
+  if type(args) ~= "table" then
+    args = {
+      args
+    }
+  end
+  return fn(input, unpack(args))
+end
+local validate
+validate = function(object, validations)
+  local errors = { }
+  local _list_0 = validations
+  for _index_0 = 1, #_list_0 do
+    local v = _list_0[_index_0]
+    local key = v[1]
+    local input = object[key]
+    for fn, args in pairs(v) do
+      local _continue_0 = false
+      repeat
+        if not (type(fn) == "string") then
+          _continue_0 = true
+          break
+        end
+        local success, msg = test_input(input, fn, args)
+        if not (success) then
+          insert(errors, msg:format(key))
+          break
+        end
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+  end
+  return next(errors) and errors
+end
+if ... == "test" then
+  require("moon")
+  local o = {
+    age = "",
+    name = "abc",
+    height = "12234234"
+  }
+  moon.p(validate(o, {
+    {
+      "age",
+      exists = true
+    },
+    {
+      "name",
+      exists = true
+    }
+  }))
+  moon.p(validate(o, {
+    {
+      "name",
+      exists = true,
+      min_length = 4
+    },
+    {
+      "age",
+      min_length = 4
+    },
+    {
+      "height",
+      max_length = 5
+    }
+  }))
+  moon.p(validate(o, {
+    {
+      "height",
+      is_integer = true
+    },
+    {
+      "name",
+      is_integer = true
+    },
+    {
+      "age",
+      is_integer = true
+    }
+  }))
+  moon.p(validate(o, {
+    {
+      "height",
+      min_length = 4
+    }
+  }))
+end
+return {
+  validate = validate,
+  test_input = test_input
+}
---
+++ lapis/validate.moon
@@ -0,0 +1,74 @@
+
+import insert from table
+
+validate_functions = {
+  exists: (input) ->
+    input and input != "", "%s must be provided"
+
+  min_length: (input, len) ->
+    #tostring(input or "") >= len, "%s must be at least #{len} chars"
+
+  max_length: (input, len) ->
+    #tostring(input or "") <= len, "%s must be at most #{len} chars"
+
+  is_integer: (input) ->
+    tostring(input)\match"^%d+$", "%s must be an integer"
+
+  equals: (input, value) ->
+    input == value, "%s must match"
+}
+
+test_input = (input, func, args) ->
+  fn = assert validate_functions[func], "Missing validation function #{func}"
+  args = {args} if type(args) != "table"
+  fn input, unpack args
+
+validate = (object, validations) ->
+  errors = {}
+  for v in *validations
+    key = v[1]
+    input = object[key]
+
+    for fn, args in pairs v
+      continue unless type(fn) == "string"
+      success, msg = test_input input, fn, args
+      unless success
+        insert errors, msg\format key
+        break
+
+  next(errors) and errors
+
+
+if ... == "test"
+  require "moon"
+
+  o = {
+    age: ""
+    name: "abc"
+    height: "12234234"
+  }
+
+  moon.p validate o, {
+    { "age", exists: true }
+    { "name", exists: true }
+  }
+
+  moon.p validate o, {
+    { "name", exists: true, min_length: 4 }
+    { "age", min_length: 4 }
+    { "height", max_length: 5 }
+  }
+
+  moon.p validate o, {
+    { "height", is_integer: true }
+    { "name", is_integer: true }
+    { "age", is_integer: true }
+  }
+
+
+  moon.p validate o, {
+    { "height", min_length: 4 }
+  }
+
+
+{ :validate, :test_input }
