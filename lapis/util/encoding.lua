2546827cb191c98b7ac1c7fb270288db04089754
leaf corcoran <leafot@gmail.com>
2013-02-18 23:38:42 -0800

    encode_with_secret/decode_with_secret

--- lapis/util/encoding.lua
+++ lapis/util/encoding.lua
@@ -9,12 +9,53 @@ else
   local b64, unb64
   do
     local _obj_0 = mime
-    b64, unb64 = _obj_0.encode_base64, _obj_0.decode_base64
+    b64, unb64 = _obj_0.b64, _obj_0.unb64
+  end
+  encode_base64 = function(...)
+    return (b64(...))
+  end
+  decode_base64 = function(...)
+    return (unb64(...))
   end
   local crypto = require("crypto")
   hmac_sha1 = function(secret, str)
     return crypto.hmac.digest("sha1", str, secret, true)
   end
+end
+local encode_with_secret
+encode_with_secret = function(object, secret, sep)
+  if sep == nil then
+    sep = "."
+  end
+  local json = require("cjson")
+  secret = secret or require("lapis.session").get_secret()
+  local msg = encode_base64(json.encode(object))
+  local signature = encode_base64(hmac_sha1(secret, msg))
+  return msg .. sep .. signature
+end
+local decode_with_secret
+decode_with_secret = function(msg_and_sig, secret)
+  local json = require("cjson")
+  secret = secret or require("lapis.session").get_secret()
+  local msg, sig = msg_and_sig:match("^(.*)%.(.*)$")
+  if not (msg) then
+    return nil, "invalid message"
+  end
+  sig = decode_base64(sig)
+  if not (sig == hmac_sha1(secret, msg)) then
+    return nil, "invalid message secret"
+  end
+  return json.decode(decode_base64(msg))
+end
+if ... == "test" then
+  require("moon")
+  local msg = encode_with_secret({
+    color = "red"
+  })
+  print(msg)
+  moon.p(decode_with_secret(msg))
+  print(decode_with_secret("hello"))
+  print(decode_with_secret("hello.world"))
 end
 return {
   encode_base64 = encode_base64,
--- lapis/util/encoding.moon
+++ lapis/util/encoding.moon
@@ -5,10 +5,44 @@ if ngx
   {:encode_base64, :decode_base64, :hmac_sha1} = ngx
 else
   mime = require "mime"
-  {encode_base64: b64, decode_base64: unb64} = mime
+  { :b64, :unb64 } = mime
+  encode_base64 = (...) -> (b64 ...)
+  decode_base64 = (...) -> (unb64 ...)
 
   crypto = require "crypto"
   hmac_sha1 = (secret, str) ->
     crypto.hmac.digest "sha1", str, secret, true
+
+encode_with_secret = (object, secret, sep=".") ->
+  json = require "cjson"
+  secret = secret or require"lapis.session".get_secret!
+
+  msg = encode_base64 json.encode object
+  signature = encode_base64 hmac_sha1 secret, msg
+  msg .. sep .. signature
+
+
+decode_with_secret = (msg_and_sig, secret) ->
+  json = require "cjson"
+  secret = secret or require"lapis.session".get_secret!
+
+  msg, sig = msg_and_sig\match "^(.*)%.(.*)$"
+  return nil, "invalid message" unless msg
+
+  sig = decode_base64 sig
+
+  unless sig == hmac_sha1(secret, msg)
+    return nil, "invalid message secret"
+
+  json.decode decode_base64 msg
+
+if ... == "test"
+  require "moon"
+  msg = encode_with_secret { color: "red" }
+  print msg
+
+  moon.p decode_with_secret msg
+  print decode_with_secret "hello"
+  print decode_with_secret "hello.world"
 
 { :encode_base64, :decode_base64, :hmac_sha1 }
