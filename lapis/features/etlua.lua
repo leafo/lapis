ed7627c8dc3e2fca4461f4fda6958c3b54e86443
leaf corcoran <leafot@gmail.com>
2014-02-08 11:59:19 -0800

    etlua baseclass

--- lapis/features/etlua.lua
+++ lapis/features/etlua.lua
@@ -63,9 +63,115 @@ do
   end
   BufferCompiler = _class_0
 end
+local EtluaWidget
+do
+  local _parent_0 = Widget
+  local _base_0 = {
+    _tpl_fn = nil,
+    content_for = function(self, name, val)
+      if val then
+        return _parent_0.content_for(self, name, val)
+      else
+        do
+          val = self[name]
+          if val then
+            self._buffer:write(val)
+            return ""
+          end
+        end
+      end
+    end,
+    _find_helper = function(self, name)
+      do
+        local chain = self:_get_helper_chain()
+        if chain then
+          for _index_0 = 1, #chain do
+            local h = chain[_index_0]
+            local helper_val = h[name]
+            if helper_val ~= nil then
+              local value
+              if type(helper_val) == "function" then
+                value = function(...)
+                  return helper_val(h, ...)
+                end
+              else
+                value = helper_val
+              end
+              return value
+            end
+          end
+        end
+      end
+      local val = self[name]
+      if val ~= nil then
+        local real_value
+        if type(val) == "function" then
+          real_value = function(...)
+            return val(self, ...)
+          end
+        else
+          real_value = val
+        end
+        return real_value
+      end
+    end,
+    render = function(self, buffer)
+      if buffer.__class == Buffer then
+        self._buffer = buffer
+      else
+        self._buffer = Buffer(buffer)
+      end
+      local seen_helpers = { }
+      local scope = setmetatable({ }, {
+        __index = function(scope, key)
+          if not seen_helpers[key] then
+            seen_helpers[key] = true
+            local helper_value = self:_find_helper(key)
+            if helper_value ~= nil then
+              scope[key] = helper_value
+              return helper_value
+            end
+          end
+        end
+      })
+      local clone = locked_fn(self._tpl_fn)
+      parser:run(clone, scope, self._buffer)
+      release_fn(clone)
+      return nil
+    end
+  }
+  _base_0.__index = _base_0
+  setmetatable(_base_0, _parent_0.__base)
+  local _class_0 = setmetatable({
+    __init = function(self, ...)
+      return _parent_0.__init(self, ...)
+    end,
+    __base = _base_0,
+    __name = "EtluaWidget",
+    __parent = _parent_0
+  }, {
+    __index = function(cls, name)
+      local val = rawget(_base_0, name)
+      if val == nil then
+        return _parent_0[name]
+      else
+        return val
+      end
+    end,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
+  })
+  _base_0.__class = _class_0
+  if _parent_0.__inherited then
+    _parent_0.__inherited(_parent_0, _class_0)
+  end
+  EtluaWidget = _class_0
+end
 return loadkit.register("etlua", function(file, mod, fname)
   local lua_code, err = parser:compile_to_lua(file:read("*a"), BufferCompiler)
-  print(lua_code)
   local fn
   if not (err) then
     fn, err = parser:load(lua_code)
@@ -75,80 +181,9 @@ return loadkit.register("etlua", function(file, mod, fname)
   end
   local TemplateWidget
   do
-    local _parent_0 = Widget
+    local _parent_0 = EtluaWidget
     local _base_0 = {
-      content_for = function(self, name, val)
-        if val then
-          return _parent_0.content_for(self, name, val)
-        else
-          do
-            val = self[name]
-            if val then
-              self._buffer:write(val)
-              return ""
-            end
-          end
-        end
-      end,
-      _find_helper = function(self, name)
-        do
-          local chain = self:_get_helper_chain()
-          if chain then
-            for _index_0 = 1, #chain do
-              local h = chain[_index_0]
-              local helper_val = h[name]
-              if helper_val ~= nil then
-                local value
-                if type(helper_val) == "function" then
-                  value = function(...)
-                    return helper_val(h, ...)
-                  end
-                else
-                  value = helper_val
-                end
-                return value
-              end
-            end
-          end
-        end
-        local val = self[name]
-        if val ~= nil then
-          local real_value
-          if type(val) == "function" then
-            real_value = function(...)
-              return val(self, ...)
-            end
-          else
-            real_value = val
-          end
-          return real_value
-        end
-      end,
-      render = function(self, buffer)
-        if buffer.__class == Buffer then
-          self._buffer = buffer
-        else
-          self._buffer = Buffer(buffer)
-        end
-        local seen_helpers = { }
-        local scope = setmetatable({ }, {
-          __index = function(scope, key)
-            if not seen_helpers[key] then
-              seen_helpers[key] = true
-              local helper_value = self:_find_helper(key)
-              if helper_value ~= nil then
-                scope[key] = helper_value
-                return helper_value
-              end
-            end
-          end
-        })
-        local clone = locked_fn(fn)
-        parser:run(clone, scope, self._buffer)
-        require("moon").p(self._buffer)
-        release_fn(clone)
-        return nil
-      end
+      _tpl_fn = fn
     }
     _base_0.__index = _base_0
     setmetatable(_base_0, _parent_0.__base)
--- lapis/features/etlua.moon
+++ lapis/features/etlua.moon
@@ -21,68 +21,70 @@ class BufferCompiler extends Compiler
     @push "_b_buffer[_b_i] = ", ...
     @push "\n" if ...
 
+class EtluaWidget extends Widget
+  _tpl_fn: nil -- set by superclass
+
+  content_for: (name, val) =>
+    if val
+      super name, val
+    else
+      if val = @[name]
+        @_buffer\write val
+        ""
+
+  _find_helper: (name) =>
+    if chain = @_get_helper_chain!
+      for h in *chain
+        helper_val = h[name]
+        if helper_val != nil
+          -- call functions in scope of helper
+          value = if type(helper_val) == "function"
+            (...) -> helper_val h, ...
+          else
+            helper_val
+
+          return value
+
+    -- look on self
+    val = @[name]
+    if val != nil
+      real_value = if type(val) == "function"
+        (...) -> val @, ...
+      else
+        val
+
+      return real_value
+
+  render: (buffer) =>
+    @_buffer = if buffer.__class == Buffer
+      buffer
+    else
+      Buffer buffer
+
+    seen_helpers = {}
+    scope = setmetatable { }, {
+      __index: (scope, key) ->
+        if not seen_helpers[key]
+          seen_helpers[key] = true
+          helper_value = @_find_helper key
+          if helper_value != nil
+            scope[key] = helper_value
+            return helper_value
+    }
+
+    clone = locked_fn @_tpl_fn
+    parser\run clone, scope, @_buffer
+    release_fn clone
+    nil
+
 loadkit.register "etlua", (file, mod, fname) ->
   lua_code, err = parser\compile_to_lua file\read("*a"), BufferCompiler
-  print lua_code
-
   fn, err = unless err
     parser\load lua_code
 
   if err
     error "[#{fname}] #{err}"
 
-  class TemplateWidget extends Widget
-    content_for: (name, val) =>
-      if val
-        super name, val
-      else
-        if val = @[name]
-          @_buffer\write val
-          ""
-
-    _find_helper: (name) =>
-      if chain = @_get_helper_chain!
-        for h in *chain
-          helper_val = h[name]
-          if helper_val != nil
-            -- call functions in scope of helper
-            value = if type(helper_val) == "function"
-              (...) -> helper_val h, ...
-            else
-              helper_val
-
-            return value
-
-      -- look on self
-      val = @[name]
-      if val != nil
-        real_value = if type(val) == "function"
-          (...) -> val @, ...
-        else
-          val
-
-        return real_value
-
-    render: (buffer) =>
-      @_buffer = if buffer.__class == Buffer
-        buffer
-      else
-        Buffer buffer
-
-      seen_helpers = {}
-      scope = setmetatable { }, {
-        __index: (scope, key) ->
-          if not seen_helpers[key]
-            seen_helpers[key] = true
-            helper_value = @_find_helper key
-            if helper_value != nil
-              scope[key] = helper_value
-              return helper_value
-      }
-
-      clone = locked_fn fn
-      parser\run clone, scope, @_buffer
-      require("moon").p @_buffer
-      release_fn clone
-      nil
+  class TemplateWidget extends EtluaWidget
+    _tpl_fn: fn
 
