c1156e69b047c0c3ca8022f3a5c371268f27bcc3
leaf corcoran <leafot@gmail.com>
2014-07-20 01:54:24 -0700

    strip tabs

--- example.lua
+++ example.lua
@@ -4,16 +4,16 @@ local app = lapis.Application()
 app:enable("etlua")
 
 app:get("index", "/", function(self)
-	-- renders views/index.etlua
-	return { render = true }
+  -- renders views/index.etlua
+  return { render = true }
 end)
 
 app:get("/user/:name", function(self)
-	return "Welcome to " .. self.params.name .. "'s profile"
+  return "Welcome to " .. self.params.name .. "'s profile"
 end)
 
 app:get("/test.json", function(self)
-	return { json = { status = "ok" } }
+  return { json = { status = "ok" } }
 end)
 
 return app
