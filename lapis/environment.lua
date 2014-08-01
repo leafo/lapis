e8143354a78dcd291a687f29b2cf27d0504e8448
leaf corcoran <leafot@gmail.com>
2014-08-01 09:31:02 -0700

    refactor env push/pop, remove its db configuration

--- lapis/environment.lua
+++ lapis/environment.lua
@@ -3,53 +3,28 @@ do
   local _obj_0 = require("lapis.cmd.util")
   default_environment = _obj_0.default_environment
 end
-local old_getter, old_backend
+local popper
 local push
 push = function(name)
   if name == nil then
     name = default_environment()
   end
-  assert(not old_getter, "environment already pushed")
+  print("PUSHING", name)
   local config_module = require("lapis.config")
-  old_getter = config_module.get
+  local old_getter = config_module.get
   local config = old_getter(name)
   config_module.get = function()
     return config
   end
-  local pg_config = config.postgres
-  if pg_config and pg_config.backend == "pgmoon" then
-    local logger = require("lapis.db").get_logger()
-    if not (os.getenv("LAPIS_SHOW_QUERIES")) then
-      logger = nil
-    end
-    local db = require("lapis.nginx.postgres")
-    local Postgres
-    do
-      local _obj_0 = require("pgmoon")
-      Postgres = _obj_0.Postgres
-    end
-    local pgmoon
-    old_backend = db.set_backend("raw", function(...)
-      if not (pgmoon) then
-        pgmoon = Postgres(pg_config)
-        assert(pgmoon:connect())
-      end
-      if logger then
-        logger.query(...)
-      end
-      return assert(pgmoon:query(...))
-    end)
+  local old_popper = popper
+  popper = function()
+    config_module.get = old_getter
+    popper = old_popper
   end
 end
 local pop
 pop = function()
-  assert(old_getter())
-  local config_module = require("lapis.config")
-  config_module.get = old_getter
-  old_getter = nil
-  local db = require("lapis.nginx.postgres")
-  db.set_backend("raw", old_backend)
-  old_backend = nil
+  return assert(popper, "no environment pushed")()
 end
 return {
   push = push,
--- lapis/environment.moon
+++ lapis/environment.moon
@@ -1,45 +1,26 @@
 
 import default_environment from require "lapis.cmd.util"
 
-local old_getter, old_backend
+local popper
 
 -- ensure that everything runs in test env, sets up db to execute queries
 push = (name=default_environment!) ->
-  assert not old_getter, "environment already pushed"
-
   config_module = require("lapis.config")
   old_getter = config_module.get
+
   config = old_getter name
-  config_module.get = -> config
+  config_module.get = (...) ->
+    if ...
+      old_getter ...
+    else
+      config
 
-  -- TODO: make this part of setting default backend
-  pg_config = config.postgres
-  if pg_config and pg_config.backend == "pgmoon"
-    logger = require("lapis.db").get_logger!
-    logger = nil unless os.getenv "LAPIS_SHOW_QUERIES"
-
-    db = require "lapis.nginx.postgres"
-    import Postgres from require "pgmoon"
-
-    local pgmoon
-
-    old_backend = db.set_backend "raw", (...) ->
-      unless pgmoon
-        pgmoon = Postgres pg_config
-        assert pgmoon\connect!
-
-      logger.query ... if logger
-      assert pgmoon\query ...
+  old_popper = popper
+  popper = ->
+    config_module.get = old_getter
+    popper = old_popper
 
 pop = ->
-  assert old_getter!
-
-  config_module = require("lapis.config")
-  config_module.get = old_getter
-  old_getter = nil
-
-  db = require "lapis.nginx.postgres"
-  db.set_backend "raw", old_backend
-  old_backend = nil
+  assert(popper, "no environment pushed")!
 
 { :push, :pop }
