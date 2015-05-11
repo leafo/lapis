6cb5f81ef47c105d7c3d4873b02a0abac6e69977
leaf corcoran <leafot@gmail.com>
2015-05-11 00:09:46 -0700

    turn spec server into class parameterized by runner

--- lapis/spec/server.lua
+++ lapis/spec/server.lua
@@ -8,126 +8,167 @@ do
   local _obj_0 = require("lapis.util")
   parse_query_string, encode_query_string = _obj_0.parse_query_string, _obj_0.encode_query_string
 end
-local current_server = nil
-local load_test_server
-load_test_server = function()
-  local attach_server
-  attach_server = require("lapis.cmd.nginx").attach_server
-  local get_free_port
-  get_free_port = require("lapis.cmd.util").get_free_port
-  local app_port = get_free_port()
-  current_server = attach_server(TEST_ENV, {
-    port = app_port
+local SpecServer
+do
+  local _base_0 = {
+    current_server = nil,
+    load_test_server = function(self)
+      local get_free_port
+      get_free_port = require("lapis.cmd.util").get_free_port
+      local app_port = get_free_port()
+      self.current_server = self.runner:attach_server(TEST_ENV, {
+        port = app_port
+      })
+      self.current_server.app_port = app_port
+      return self.current_server
+    end,
+    close_test_server = function(self)
+      self.runner:detach_server()
+      self.current_server = nil
+    end,
+    get_current_server = function(self)
+      return self.current_server
+    end,
+    request = function(self, path, opts)
+      if path == nil then
+        path = ""
+      end
+      if opts == nil then
+        opts = { }
+      end
+      if not (self.current_server) then
+        error("The test server is not loaded! (did you forget to load_test_server?)")
+      end
+      local http = require("socket.http")
+      local headers = { }
+      local method = opts.method
+      local port = opts.port or self.current_server.app_port
+      local source
+      do
+        local data = opts.post or opts.data
+        if data then
+          if opts.post then
+            method = method or "POST"
+          end
+          if type(data) == "table" then
+            headers["Content-type"] = "application/x-www-form-urlencoded"
+            data = encode_query_string(data)
+          end
+          headers["Content-length"] = #data
+          source = ltn12.source.string(data)
+        end
+      end
+      local url_host, url_path = path:match("^https?://([^/]+)(.*)$")
+      if url_host then
+        headers.Host = url_host
+        path = url_path
+        do
+          local override_port = url_host:match(":(%d+)$")
+          if override_port then
+            port = override_port
+          end
+        end
+      end
+      path = path:gsub("^/", "")
+      if opts.get then
+        local _, url_query = path:match("^(.-)%?(.*)$")
+        local get_params
+        if url_query then
+          get_params = parse_query_string(url_query)
+        else
+          get_params = { }
+        end
+        for k, v in pairs(opts.get) do
+          get_params[k] = v
+        end
+        path = path:gsub("^.-(%?.*)$", "") .. "?" .. encode_query_string(get_params)
+      end
+      if opts.headers then
+        for k, v in pairs(opts.headers) do
+          headers[k] = v
+        end
+      end
+      local buffer = { }
+      local res, status
+      res, status, headers = http.request({
+        url = "http://127.0.0.1:" .. tostring(port) .. "/" .. tostring(path),
+        redirect = false,
+        sink = ltn12.sink.table(buffer),
+        headers = headers,
+        method = method,
+        source = source
+      })
+      assert(res, status)
+      local body = table.concat(buffer)
+      headers = normalize_headers(headers)
+      if headers.x_lapis_error then
+        json = require("cjson")
+        local err, trace
+        do
+          local _obj_0 = json.decode(body)
+          status, err, trace = _obj_0.status, _obj_0.err, _obj_0.trace
+        end
+        error("\n" .. tostring(status) .. "\n" .. tostring(err) .. "\n" .. tostring(trace))
+      end
+      if opts.expect == "json" then
+        json = require("cjson")
+        if not (pcall(function()
+          body = json.decode(body)
+        end)) then
+          error("expected to get json from " .. tostring(path))
+        end
+      end
+      return status, body, headers
+    end
+  }
+  _base_0.__index = _base_0
+  local _class_0 = setmetatable({
+    __init = function(self, runner)
+      self.runner = runner
+      self.runner = self.runner or require("lapis.cmd.nginx").nginx_runner
+    end,
+    __base = _base_0,
+    __name = "SpecServer"
+  }, {
+    __index = _base_0,
+    __call = function(cls, ...)
+      local _self_0 = setmetatable({}, _base_0)
+      cls.__init(_self_0, ...)
+      return _self_0
+    end
   })
-  current_server.app_port = app_port
-  return current_server
+  _base_0.__class = _class_0
+  SpecServer = _class_0
 end
-local close_test_server
-close_test_server = function()
-  local detach_server
-  detach_server = require("lapis.cmd.nginx").detach_server
-  detach_server()
-  current_server = nil
-end
-local get_current_server
-get_current_server = function()
-  return current_server
-end
-local request
-request = function(path, opts)
-  if path == nil then
-    path = ""
-  end
-  if opts == nil then
-    opts = { }
-  end
-  if not (current_server) then
-    error("The test server is not loaded! (did you forget to load_test_server?)")
-  end
-  local http = require("socket.http")
-  local headers = { }
-  local method = opts.method
-  local port = opts.port or current_server.app_port
-  local source
-  do
-    local data = opts.post or opts.data
-    if data then
-      if opts.post then
-        method = method or "POST"
-      end
-      if type(data) == "table" then
-        headers["Content-type"] = "application/x-www-form-urlencoded"
-        data = encode_query_string(data)
-      end
-      headers["Content-length"] = #data
-      source = ltn12.source.string(data)
-    end
-  end
-  local url_host, url_path = path:match("^https?://([^/]+)(.*)$")
-  if url_host then
-    headers.Host = url_host
-    path = url_path
-    do
-      local override_port = url_host:match(":(%d+)$")
-      if override_port then
-        port = override_port
-      end
-    end
-  end
-  path = path:gsub("^/", "")
-  if opts.get then
-    local _, url_query = path:match("^(.-)%?(.*)$")
-    local get_params
-    if url_query then
-      get_params = parse_query_string(url_query)
-    else
-      get_params = { }
-    end
-    for k, v in pairs(opts.get) do
-      get_params[k] = v
-    end
-    path = path:gsub("^.-(%?.*)$", "") .. "?" .. encode_query_string(get_params)
-  end
-  if opts.headers then
-    for k, v in pairs(opts.headers) do
-      headers[k] = v
-    end
-  end
-  local buffer = { }
-  local res, status
-  res, status, headers = http.request({
-    url = "http://127.0.0.1:" .. tostring(port) .. "/" .. tostring(path),
-    redirect = false,
-    sink = ltn12.sink.table(buffer),
-    headers = headers,
-    method = method,
-    source = source
-  })
-  assert(res, status)
-  local body = table.concat(buffer)
-  headers = normalize_headers(headers)
-  if headers.x_lapis_error then
-    json = require("cjson")
-    local err, trace
-    do
-      local _obj_0 = json.decode(body)
-      status, err, trace = _obj_0.status, _obj_0.err, _obj_0.trace
-    end
-    error("\n" .. tostring(status) .. "\n" .. tostring(err) .. "\n" .. tostring(trace))
-  end
-  if opts.expect == "json" then
-    json = require("cjson")
-    if not (pcall(function()
-      body = json.decode(body)
-    end)) then
-      error("expected to get json from " .. tostring(path))
-    end
-  end
-  return status, body, headers
-end
+local default_server = SpecServer()
 return {
-  load_test_server = load_test_server,
-  close_test_server = close_test_server,
-  get_current_server = get_current_server,
-  request = request
+  SpecServer = SpecServer,
+  load_test_server = (function()
+    local _base_0 = default_server
+    local _fn_0 = _base_0.load_test_server
+    return function(...)
+      return _fn_0(_base_0, ...)
+    end
+  end)(),
+  close_test_server = (function()
+    local _base_0 = default_server
+    local _fn_0 = _base_0.close_test_server
+    return function(...)
+      return _fn_0(_base_0, ...)
+    end
+  end)(),
+  get_current_server = (function()
+    local _base_0 = default_server
+    local _fn_0 = _base_0.get_current_server
+    return function(...)
+      return _fn_0(_base_0, ...)
+    end
+  end)(),
+  request = (function()
+    local _base_0 = default_server
+    local _fn_0 = _base_0.request
+    return function(...)
+      return _fn_0(_base_0, ...)
+    end
+  end)()
 }
--- lapis/spec/server.moon
+++ lapis/spec/server.moon
@@ -7,103 +7,107 @@ json = require "cjson"
 
 import parse_query_string, encode_query_string from require "lapis.util"
 
-current_server = nil
+class SpecServer
+  current_server: nil
 
-load_test_server = ->
-  import attach_server from require "lapis.cmd.nginx"
-  import get_free_port from require "lapis.cmd.util"
+  new: (@runner) =>
+    @runner or= require("lapis.cmd.nginx").nginx_runner
 
-  app_port = get_free_port!
+  load_test_server: =>
+    import get_free_port from require "lapis.cmd.util"
+    app_port = get_free_port!
 
-  current_server = attach_server TEST_ENV, { port: app_port }
-  current_server.app_port = app_port
-  current_server
+    @current_server = @runner\attach_server TEST_ENV, { port: app_port }
+    @current_server.app_port = app_port
+    @current_server
 
-close_test_server = ->
-  import detach_server from require "lapis.cmd.nginx"
-  detach_server!
+  close_test_server: =>
+    @runner\detach_server!
+    @current_server = nil
 
-  current_server = nil
+  get_current_server: =>
+    @current_server
 
-get_current_server = ->
-  current_server
+  -- hits the server in test environment
+  request: (path="", opts={}) =>
+    unless @current_server
+      error "The test server is not loaded! (did you forget to load_test_server?)"
 
--- hits the server in test environment
-request = (path="", opts={}) ->
-  unless current_server
-    error "The test server is not loaded! (did you forget to load_test_server?)"
+    http = require "socket.http"
 
-  http = require "socket.http"
+    headers = {}
+    method = opts.method
+    port = opts.port or @current_server.app_port
 
-  headers = {}
-  method = opts.method
-  port = opts.port or current_server.app_port
+    source = if data = opts.post or opts.data
+      method or= "POST" if opts.post
 
-  source = if data = opts.post or opts.data
-    method or= "POST" if opts.post
+      if type(data) == "table"
+        headers["Content-type"] = "application/x-www-form-urlencoded"
+        data = encode_query_string data
 
-    if type(data) == "table"
-      headers["Content-type"] = "application/x-www-form-urlencoded"
-      data = encode_query_string data
+      headers["Content-length"] = #data
+      ltn12.source.string(data)
 
-    headers["Content-length"] = #data
-    ltn12.source.string(data)
+    -- if the path is a url then extract host and path
+    url_host, url_path = path\match "^https?://([^/]+)(.*)$"
+    if url_host
+      headers.Host = url_host
+      path = url_path
+      if override_port = url_host\match ":(%d+)$"
+        port = override_port
 
-  -- if the path is a url then extract host and path
-  url_host, url_path = path\match "^https?://([^/]+)(.*)$"
-  if url_host
-    headers.Host = url_host
-    path = url_path
-    if override_port = url_host\match ":(%d+)$"
-      port = override_port
+    path = path\gsub "^/", ""
 
-  path = path\gsub "^/", ""
+    -- merge get parameters
+    if opts.get
+      _, url_query = path\match "^(.-)%?(.*)$"
+      get_params = if url_query
+        parse_query_string url_query
+      else
+        {}
 
-  -- merge get parameters
-  if opts.get
-    _, url_query = path\match "^(.-)%?(.*)$"
-    get_params = if url_query
-      parse_query_string url_query
-    else
-      {}
+      for k,v in pairs opts.get
+        get_params[k] = v
 
-    for k,v in pairs opts.get
-      get_params[k] = v
+      path = path\gsub("^.-(%?.*)$", "") .. "?" .. encode_query_string get_params
 
-    path = path\gsub("^.-(%?.*)$", "") .. "?" .. encode_query_string get_params
+    if opts.headers
+      for k,v in pairs opts.headers
+        headers[k] = v
 
-  if opts.headers
-    for k,v in pairs opts.headers
-      headers[k] = v
+    buffer = {}
+    res, status, headers = http.request {
+      url: "http://127.0.0.1:#{port}/#{path}"
+      redirect: false
+      sink: ltn12.sink.table buffer
+      :headers, :method, :source
+    }
 
-  buffer = {}
-  res, status, headers = http.request {
-    url: "http://127.0.0.1:#{port}/#{path}"
-    redirect: false
-    sink: ltn12.sink.table buffer
-    :headers, :method, :source
-  }
+    assert res, status
+    body = table.concat buffer
 
-  assert res, status
-  body = table.concat buffer
+    headers = normalize_headers headers
+    if headers.x_lapis_error
+      json = require "cjson"
+      {:status, :err, :trace} = json.decode body
+      error "\n#{status}\n#{err}\n#{trace}"
 
-  headers = normalize_headers headers
-  if headers.x_lapis_error
-    json = require "cjson"
-    {:status, :err, :trace} = json.decode body
-    error "\n#{status}\n#{err}\n#{trace}"
+    if opts.expect == "json"
+      json = require "cjson"
+      unless pcall -> body = json.decode body
+        error "expected to get json from #{path}"
 
-  if opts.expect == "json"
-    json = require "cjson"
-    unless pcall -> body = json.decode body
-      error "expected to get json from #{path}"
+    status, body, headers
 
-  status, body, headers
+default_server = SpecServer!
 
 {
-  :load_test_server
-  :close_test_server
-  :get_current_server
-  :request
+  :SpecServer
+
+  load_test_server: default_server\load_test_server
+  close_test_server: default_server\close_test_server
+  get_current_server: default_server\get_current_server
+  request: default_server\request
 }
 
