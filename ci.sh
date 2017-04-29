3ece504eab1291b62655f2bd982007aae759c125
leaf corcoran <leafot@gmail.com>
2017-04-28 17:55:47 -0700

    comment out mysql for now

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ luarocks-5.1 install lpeg 0.10.2
 luarocks-5.1 install moonscript
 luarocks-5.1 install luaposix
 luarocks-5.1 install date
-luarocks-5.1 install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
+# luarocks-5.1 install luasql-mysql MYSQL_INCDIR=/usr/include/mysql
 luarocks-5.1 make
 
 
