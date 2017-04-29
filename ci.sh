a5860c41f6f42f92f84a1fed48269cf39e5e5f52
leaf corcoran <leafot@gmail.com>
2017-04-28 18:33:44 -0700

    run postgres specs

--- ci.sh
+++ ci.sh
@@ -24,5 +24,7 @@ echo "full_page_writes = off" >> /var/lib/postgres/data/postgresql.conf
 su postgres -c '/usr/bin/pg_ctl -s -D /var/lib/postgres/data start -w -t 120'
 
 make build
+make test_db
 
 ./busted -o utfTerminal
+./busted -o utfTerminal spec_postgres/
