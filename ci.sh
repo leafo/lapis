b408705c1d8822814716fdfe5b7cd9bfd87d6aba
leaf corcoran <leafot@gmail.com>
2017-07-19 20:47:31 -0700

    specs run (maybe?)

--- ci.sh
+++ ci.sh
@@ -5,6 +5,9 @@ set -o xtrace
 
 eval $(luarocks-5.1 path)
 
+# add openresty
+export LUA_PATH="$LUA_PATH;/usr/local/openresty/lualib/?.lua"
+
 # setup busted
 cat $(which busted) | sed 's/\/usr\/bin\/lua5\.1/\/usr\/bin\/luajit/' > busted
 chmod +x busted
@@ -15,8 +18,13 @@ echo "synchronous_commit = off" >> /var/lib/postgres/data/postgresql.conf
 echo "full_page_writes = off" >> /var/lib/postgres/data/postgresql.conf
 su postgres -c '/usr/bin/pg_ctl -s -D /var/lib/postgres/data start -w -t 120'
 
+# start mysql
+mkdir -p /run/mysqld
+/usr/bin/mysqld --user=root --basedir=/usr --datadir=/var/lib/mysql &
+
 make build
 make test_db
+make mysql_test_db
 
 ./busted -o utfTerminal
 ./busted -o utfTerminal spec_postgres/
