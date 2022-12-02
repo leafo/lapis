5b4c23e281aed6867fe0c0b3d8a20c26ae25db7b
leaf corcoran <leafot@gmail.com>
2022-12-02 13:50:46 -0800

    turn old lapis.nginx.postgres module into error instead of warning

--- lapis/nginx/postgres.lua
+++ lapis/nginx/postgres.lua
@@ -1,2 +1,1 @@
-io.stderr:write("WARNING: The module `lapis.nginx.postgres` has moved to `lapis.db.postgres`\n  Please update your require statements as the old path will no longer be\n  available in future versions of lapis.\n\n")
-return require("lapis.db.postgres")
+return error("The module `lapis.nginx.postgres` has moved to `lapis.db.postgres`\nPlease update your require statements as the old path is no longer available.")
--- lapis/nginx/postgres.moon
+++ lapis/nginx/postgres.moon
@@ -1,6 +1,1 @@
-io.stderr\write "WARNING: The module `lapis.nginx.postgres` has moved to `lapis.db.postgres`
-  Please update your require statements as the old path will no longer be
-  available in future versions of lapis.\n\n"
-
-require "lapis.db.postgres"
-
+error "The module `lapis.nginx.postgres` has moved to `lapis.db.postgres`\nPlease update your require statements as the old path is no longer available."
