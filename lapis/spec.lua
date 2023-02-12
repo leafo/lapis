eb5cbc116e732f5599dcb30092b51b014e49282e
leaf corcoran <leafot@gmail.com>
2023-02-12 12:51:16 -0800

    remove undocumented outdated spec helpers

--- lapis/spec.lua
+++ lapis/spec.lua
@@ -1,29 +1,3 @@
-local use_db_connection
-use_db_connection = function()
-  local setup, teardown
-  do
-    local _obj_0 = require("busted")
-    setup, teardown = _obj_0.setup, _obj_0.teardown
-  end
-  local config = require("lapis.config").get()
-  if not (config.postgres or config.mysql) then
-    return 
-  end
-  setup(function()
-    local connect
-    connect = require("lapis.db").connect
-    if connect then
-      return connect()
-    end
-  end)
-  return teardown(function()
-    local disconnect
-    disconnect = require("lapis.db").disconnect
-    if disconnect then
-      return disconnect()
-    end
-  end)
-end
 local use_test_env
 use_test_env = function(env_name)
   if env_name == nil then
@@ -38,10 +12,9 @@ use_test_env = function(env_name)
   setup(function()
     return env.push(env_name)
   end)
-  teardown(function()
+  return teardown(function()
     return env.pop()
   end)
-  return use_db_connection()
 end
 local use_test_server
 use_test_server = function()
@@ -58,28 +31,9 @@ use_test_server = function()
   setup(function()
     return load_test_server()
   end)
-  teardown(function()
+  return teardown(function()
     return close_test_server()
   end)
-  return use_db_connection()
-end
-local assert_no_queries
-assert_no_queries = function(fn)
-  if fn == nil then
-    fn = error("missing function")
-  end
-  local assert = require("luassert")
-  local db = require("lapis.db")
-  local old_query = db.get_raw_query()
-  local query_log = { }
-  db.set_raw_query(function(...)
-    table.insert(query_log, (...))
-    return old_query(...)
-  end)
-  local res, err = pcall(fn)
-  db.set_raw_query(old_query)
-  assert(res, err)
-  return assert.same({ }, query_log)
 end
 local running_in_test
 running_in_test = function()
@@ -92,6 +46,5 @@ end
 return {
   use_test_env = use_test_env,
   use_test_server = use_test_server,
-  assert_no_queries = assert_no_queries,
   running_in_test = running_in_test
 }
--- lapis/spec.moon
+++ lapis/spec.moon
@@ -1,18 +1,4 @@
 
-use_db_connection = ->
-  import setup, teardown from require "busted"
-
-  config = require("lapis.config").get!
-  return unless config.postgres or config.mysql
-
-  setup ->
-    import connect from require "lapis.db"
-    connect! if connect
-
-  teardown ->
-    import disconnect from require "lapis.db"
-    disconnect! if disconnect
-
 use_test_env = (env_name="test") ->
   import setup, teardown from require "busted"
   env = require "lapis.environment"
@@ -20,33 +6,12 @@ use_test_env = (env_name="test") ->
   setup -> env.push env_name
   teardown -> env.pop!
 
-  use_db_connection!
-
 use_test_server = ->
   import setup, teardown from require "busted"
   import load_test_server, close_test_server from require "lapis.spec.server"
 
   setup -> load_test_server!
   teardown -> close_test_server!
-
-  use_db_connection!
-
-assert_no_queries = (fn=error"missing function") ->
-  assert = require "luassert"
-  db = require "lapis.db"
-
-  old_query = db.get_raw_query!
-
-  query_log = {}
-  db.set_raw_query (...) ->
-    table.insert query_log, (...)
-    old_query ...
-
-  res, err = pcall fn
-  db.set_raw_query old_query
-  assert res, err
-  assert.same {}, query_log
-
 
 -- used to determine if default environment should be test intead of development
 -- should return the name of the test harness being used, otherwise false
@@ -57,4 +22,4 @@ running_in_test = ->
 
   false
 
-{:use_test_env, :use_test_server, :assert_no_queries, :running_in_test}
+{:use_test_env, :use_test_server, :running_in_test}
